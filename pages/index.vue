4707472089ecd31bc108d39ec13ea968930cd8e0
SALA 2-14 <jeffgrijalba6@gmail.com>
2019-03-21 20:28:31 -0500

    Iniciando proyecto

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Hola {{ msg }}</h1>
+    <input type="text" v-model="msg">
+    <button @click="guardar">Guardar</button>
+    <table>
+      <tr>
+        <th>Nombre</th>
+      </tr>
+      <tr v-for="(mun, index) in municipios" :key="index">
+        <td>{{ mun }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Jeff Grijalba",
+      municipios: ["mocoa", "valle del guamuez", "pto asis"]
+    };
+  },
+  methods: {
+      guardar(){
+          this.municipios.push(this.msg)
+          this.msg = ''
+      }
+  }
+};
+</script>
+
+<style>
+</style>
