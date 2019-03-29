367a4d57b47f840e071161bb268e35c3330ab5c0
SALA 2-14 <jeffgrijalba6@gmail.com>
2019-03-28 20:28:43 -0500

    creando firebase

---
+++ services/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyA_yxWzuTtL4GKOqi4auyR88oug6Px4oTw",
+    authDomain: "nuxt-64a0d.firebaseapp.com",
+    databaseURL: "https://nuxt-64a0d.firebaseio.com",
+    projectId: "nuxt-64a0d",
+    storageBucket: "nuxt-64a0d.appspot.com",
+    messagingSenderId: "337466319950"
+  };
+  firebase.initializeApp(config);
+
+  export default firebase
