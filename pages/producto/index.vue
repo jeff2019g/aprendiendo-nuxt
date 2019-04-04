86ad0f519d7d373c62d426093c3ea3deb6b0b6ac
SALA 2-14 <jeffgrijalba6@gmail.com>
2019-04-03 22:45:58 -0500

    crear base de datos firebase

--- pages/producto/index.vue
+++ pages/producto/index.vue
@@ -1,11 +1,43 @@
 <template>
-    <div class="row mt-3">
-        <div class="col-sm-6">
-            <h2>Listado de Productos</h2>
-            
-        </div>
-        <div class="col-sm-6">
-            <b-button variant="primary" href="/producto/crear">Nuevo</b-button>
-        </div>
+  <div class="row mt-3">
+    <div class="col-sm-6">
+      <h2>Listado de Productos</h2>
     </div>
+
+    <div class="col-sm-6">
+      <b-button variant="primary" href="/producto/crear">Nuevo</b-button>
+    </div>
+
+    <div class="rown mt-2">
+      <div class="col-sm-12">
+        <b-table responsive striped hover :fields="fields" :items="productos"/>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import { db } from "../../services/firebase";
+export default {
+  asyncData() {
+   return db.collection("productos")
+      .get()
+      .then(productosSnap => {
+        let productos = [];
+
+        productosSnap.forEach(value => {
+            productos.push(value.data())
+        })
+        return{
+            productos
+        }
+      })
+  },
+  data() {
+    return {
+      fields: ["imagen", "nombre", "precio", "cantidad", "acciones"]
+    };
+  }
+};
+</script>
+
--- services/firebase.js
+++ services/firebase.js
@@ -1,13 +1,21 @@
 import firebase from 'firebase'
 
 var config = {
-    apiKey: "AIzaSyA_yxWzuTtL4GKOqi4auyR88oug6Px4oTw",
-    authDomain: "nuxt-64a0d.firebaseapp.com",
-    databaseURL: "https://nuxt-64a0d.firebaseio.com",
-    projectId: "nuxt-64a0d",
-    storageBucket: "nuxt-64a0d.appspot.com",
-    messagingSenderId: "337466319950"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyA_yxWzuTtL4GKOqi4auyR88oug6Px4oTw",
+  authDomain: "nuxt-64a0d.firebaseapp.com",
+  databaseURL: "https://nuxt-64a0d.firebaseio.com",
+  projectId: "nuxt-64a0d",
+  storageBucket: "nuxt-64a0d.appspot.com",
+  messagingSenderId: "337466319950"
+};
 
-  export default firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const db = firebase.firestore()
+
+export default firebase
+export {
+  db
+}
